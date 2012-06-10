5048a9e9c6d8d7ea0fd119aa45d888f9cb3ee4f0
Andy Waite <github.aw@andywaite.com>
2012-06-10 01:11:24 +0100

    Fix view controller

--- app/controllers/widget_detail_controller.rb
+++ app/controllers/widget_detail_controller.rb
@@ -7,18 +7,44 @@ class WidgetDetailController < UIViewController
   end
 
   def loadView
-    widget_detail_view = WidgetDetailView.alloc.init
-    # TODO do this within the view?
-    widget_detail_view.backgroundColor = UIColor.groupTableViewBackgroundColor
-    # TODO is this a sensible approach?
-    widget_detail_view.delegate = self
-    widget_detail_view.item = @item
-    self.view = widget_detail_view
-    if @item
-      self.title = "Edit Widget"
-    else
-      self.title = "New Widget"
-    end
+    # http://cps.liridesce.com/?p=100
+    super
+    button = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemDone,
+                     target:self,
+                     action:'done')
+  self.navigationItem.setRightBarButtonItem(button)
+  #[button release];
+ 
+  frame = self.view.bounds
+ 
+  navBar = UINavigationBar.alloc.initWithFrame(frame)
+  frame.size = navBar.sizeThatFits(frame.size)
+  navBar.setFrame(frame)
+  navBar.setAutoresizingMask(UIViewAutoresizingFlexibleWidth)
+  navBar.setItems([self.navigationItem])
+ 
+  self.view.addSubview(navBar)
+ 
+ # http://stackoverflow.com/questions/1492852/add-toolbar-to-uitableviewcontroller
+
+  navHeight = 44 # TODO this is a guess
+
+  detail_view = WidgetDetailView.alloc.initWithFrame(CGRectMake(0,
+    navHeight,
+    self.view.bounds.size.width,
+    self.view.bounds.size.height-navHeight))
+    # # TODO do this within the view?
+    detail_view.backgroundColor = UIColor.groupTableViewBackgroundColor
+    # # TODO is this a sensible approach?
+    detail_view.delegate = self
+    detail_view.item = @item
+    # self.view = widget_detail_view
+     if @item
+       self.title = "Edit Widget"
+     else
+       self.title = "New Widget"
+     end
+    self.view.addSubview detail_view
   end
 
   #def viewDidLoad
