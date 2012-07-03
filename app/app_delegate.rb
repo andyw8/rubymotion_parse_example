7dbd655e5887c3b1043f12244e51c17227c31170
Andy Waite <github.aw@andywaite.com>
2012-07-03 15:47:39 +0100

    Cleaner config

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,9 +2,9 @@ class AppDelegate
   attr_reader :nav
 
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    Parse.setApplicationId(Config::PARSE_APP_ID, clientKey:Config::PARSE_CLIENT_KEY)
-    PFTwitterUtils.initializeWithConsumerKey(Config::TWITTER_CONSUMER_KEY,
-      consumerSecret:Config::TWITTER_CONSUMER_SECRET)
+    Parse.setApplicationId(Config::Parse::AppId, clientKey:Config::Parse::ClientKey)
+    PFTwitterUtils.initializeWithConsumerKey(Config::Twitter::ConsumerKey,
+      consumerSecret:Config::Twitter::ConsumerSecret)
     root_view_controller = WidgetsController.alloc.initWithStyle(UITableViewStylePlain)
     @nav = UINavigationController.alloc.initWithRootViewController(root_view_controller)
     @nav.wantsFullScreenLayout = true
